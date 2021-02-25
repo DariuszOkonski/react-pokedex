d7648b2a9bb8976dff0837a5dd3a754e96439f40
Dariusz Okonski <darek200180@gmail.com>
2021-02-25 22:52:15 +0000

    Updates

---
+++ precache-manifest.5cf781c8e753686ffb9ca4b7fd79cc48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4bef759fab30b31e324fc17c017b91d",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "8ea354acdfd226439eaf",
+    "url": "/react-pokedex/static/css/main.fb5d38bd.chunk.css"
+  },
+  {
+    "revision": "c1adc82956347686c6d3",
+    "url": "/react-pokedex/static/js/2.56f35e3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pokedex/static/js/2.56f35e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea354acdfd226439eaf",
+    "url": "/react-pokedex/static/js/main.6700cc5e.chunk.js"
+  },
+  {
+    "revision": "9115d5d0612950755ee2",
+    "url": "/react-pokedex/static/js/runtime-main.bf947c1c.js"
+  }
+]);
